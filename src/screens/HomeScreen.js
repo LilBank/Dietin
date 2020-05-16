bcc19c1ccd1d787a785f60fe073d00e225ee7363
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2020-05-16 22:55:55 +0700

    add some style

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {View, Text, Button} from 'native-base';
+import {View, Text, Button} from 'react-native';
+import {TouchableHighlight} from 'react-native-gesture-handler';
 
 export default class HomeScreen extends Component {
   constructor(props) {
@@ -17,22 +18,55 @@ export default class HomeScreen extends Component {
   render() {
     const {caloriesRemaining, caloriesConsumed, caloriesBurnOut} = this.state;
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Calories Remaining</Text>
-        <Text>{caloriesRemaining}</Text>
-        <Text>Consumed {caloriesConsumed}</Text>
-        <Text>Burn Out {caloriesBurnOut}</Text>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Button transparent>Eat</Button>
-          <Button transparent>Workout</Button>
-        </View>
+      <View style={styles.container}>
+        <Text style={{fontSize: 20, marginBottom: 30, marginTop: 30}}>
+          Calories{'\n'}Remaining
+        </Text>
+        <Text style={{fontSize: 20, marginBottom: 30}}>
+          {caloriesRemaining}
+        </Text>
+        <Text style={{fontSize: 20, marginBottom: 30}}>
+          Consumed {caloriesConsumed}
+        </Text>
+        <Text style={{fontSize: 20, marginBottom: 30}}>
+          Burn Out {caloriesBurnOut}
+        </Text>
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => {}}>
+          <Text>Eat</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => {}}>
+          <Text>Workout</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => {}}>
+          <Text>Reset</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    opacity: 0.7,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  image: {
+    width: 125,
+    height: 125,
+    marginBottom: 25,
+  },
+});
