a4c45e756dcdc3963d2bdde0fbb3f8517343ce9b
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2020-05-17 21:06:45 +0700

    modify login

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,24 +25,22 @@ export default class Login extends Component {
   };
 
   onLogin = () => {
+    const {email, password} = this.state;
     auth()
-      .createUserWithEmailAndPassword(
-        'sarah.lane@gmail.com',
-        'SuperSecretPassword!',
-      )
+      .createUserWithEmailAndPassword(email, password)
       .then(() => {
-        console.log('User account created & signed in!');
+        alert('User account created & signed in!');
       })
       .catch(error => {
         if (error.code === 'auth/email-already-in-use') {
-          console.log('That email address is already in use!');
+          alert('That email address is already in use!');
         }
 
         if (error.code === 'auth/invalid-email') {
-          console.log('That email address is invalid!');
+          alert('That email address is invalid!');
         }
 
-        console.error(error);
+        alert(error);
       });
     this.props.navigation.navigate('Home');
   };
